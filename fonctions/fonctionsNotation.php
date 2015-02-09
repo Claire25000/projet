9dca95abd146a27e8e08cc470ebce557c6c39755
dodo2525 <dododu25@hotmail.fr>
2015-02-09 22:10:48 +0100

    k

--- fonctions/fonctionsNotation.php
+++ fonctions/fonctionsNotation.php
@@ -19,4 +19,15 @@ function retourneNote($idProduit){
 	
 	return $nb;
 }
+function aDejaNote($idUtilisateur,$idProduit){
+	global $connexion; // on définie la variables globale de connection dans la fonction
+	
+	$requete = $connexion->query("SELECT * FROM `notation` WHERE idProduit = ".$idProduit." AND idUtilisateur = ".$idUtilisateur.""); 
+
+	$requete->setFetchMode(PDO::FETCH_OBJ);
+ 	if($requete->rowCount() == 1){ // si on a pas 1 résultat, l'authentification est un échec
+		return true;
+	}
+	return false;
+}
 ?>
